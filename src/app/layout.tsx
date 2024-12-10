1edd9122e907d1a83d8831ab1ed857dfdd349ee5
qieman <253058016@qq.com>
2024-12-10 16:07:33 +0800

    add tg login

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import {Toaster} from '@/components/client/shadcn/Toast/toaster'
 import Modals from '@/components/client/Modal/Modals'
 import {selectLang} from '@/app/actions'
 import Image from 'next/image'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
     title: "Social Layer",
@@ -26,12 +27,17 @@ export default async function RootLayout({
                 content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
         </Head>
         <body className={`antialiased`}>
+            <Script async src="https://telegram.org/js/telegram-widget.js?22"
+                data-telegram-login="develop_solar_auth_bot" data-size="large" data-auth-url=""
+                data-request-access="write"></Script>
+
             <div className="min-h-[100svh]">
                 <header className="w-full h-[48px] shadow sticky top-0 bg-[var(--background)] z-[999]">
                     <div className="page-width w-full flex-row-item-center justify-between items-center h-[48px]">
                         <div className="flex-row-item-center">
                             <a href="/">
-                                <Image src="/images/logo_horizontal.svg" width={102} height={32} alt="Social Layer"/>
+                                <Image src="/images/logo_horizontal.svg" width={102} height={32}
+                                    alt="Social Layer"/>
                             </a>
                         </div>
                         <div className="flex-row-item-center text-xs">
