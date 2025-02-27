5381d2221601596be67dba844b95f3ed7b58eaf7
ZOU <253058016@qq.com>
2025-02-28 02:01:55 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: "/",
+                headers: [
+                    {
+                        key: "Cross-Origin-Opener-Policy",
+                        value: 'unsafe-none',
+                    }
+                ],
+            },
+        ]
+    },
+}
 
 export default nextConfig
